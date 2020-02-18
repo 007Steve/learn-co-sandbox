d6bdbb83594231f802e3ed82364ee84363f998c7
Stephen Plummer <stephen.plummer1.sp@gmail.com>
2020-02-18 01:51:51 +0000

    7th

--- greatest/lib/greatest.rb
+++ greatest/lib/greatest.rb
@@ -2,9 +2,11 @@ require_relative "./greatest/version"
 require_relative "./greatest/cli"
 require_relative "./greatest/player"
 require_relative "./greatest/scraper"
-require 'pry'
+
+
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 module Greatest
   class Error < StandardError; end
   # Your code goes here...
--- greatest/lib/greatest/scraper.rb
+++ greatest/lib/greatest/scraper.rb
@@ -1,8 +1,9 @@
 class Greatest::Scraper
   
   def self.scraper_player
+    
   doc = Nokogiri::HTML(open("https://bleacherreport.com/articles/1795233-10-greatest-nba-players-of-all-time#slide9"))  
-  binding.pry 
+   binding.pry
 end
-
+ 
 end
